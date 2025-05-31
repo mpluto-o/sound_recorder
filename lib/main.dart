e811b264064bd09e7a806f2bdd6ce1084b14277e
Alok <adept678@gmail.com>
2025-05-31 20:18:16 +0530

    Making it look prettier

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -34,6 +39,7 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Center(
@@ -41,15 +47,37 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('An awesome idea:'),
-            Text(appState.current.asCamelCase),
+            BigCard(pair: pair),
             ElevatedButton(
               onPressed: () {
-                print('button pressed!');
+                appState.getNext();
               },
-              child: Text('Go'),
+              child: Text('Next'),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primaryContainer,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Text(pair.asCamelCase, style: style),
       ),
     );
   }
